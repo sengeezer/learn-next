738a1ab125194d4aff397d3cb1f083ca119aeff1
Fabio Albertin <fabio.albertin@dvcorporate.com>
2020-02-24 17:15:16 +0000

    Finished chapter 1

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default () => (
+    <div>
+        <Link href="/about">
+            <a title="About page">About</a>
+        </Link>
+        <p>Hello Next.js</p>
+    </div>
+);
