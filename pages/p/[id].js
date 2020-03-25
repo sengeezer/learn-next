a0abe17462d18e649d030dd75ad210a773d7cf7c
Fabio Albertin <fabio.albertin@dvcorporate.com>
2020-03-25 19:14:22 +0000

    Finished data fetching chapter

--- pages/p/[id].js
+++ pages/p/[id].js
@@ -1,13 +1,22 @@
 import Layout from '../../components/Layout';
 import fetch from 'isomorphic-unfetch';
 
-const Post = ({ show }) => {
+const Post = ({ show }) => (
+    <Layout>
+        <h1>{show.name}</h1>
+        <p>{show.summary.replace(/<[/]?[pb]>/g, '')}</p>
+        {show.image ? <img src={show.image.medium} /> : null}
+    </Layout>
+);
 
-    return (
-        <Layout>
-            <p>Blawg post content.</p>
-        </Layout>
-    );
-};
+Post.getInitialProps = async (context) => {
+    const { id } = context.query;
+    const res = await fetch(`https://api.tvmaze.com/shows/${id}`);
+    const show = await res.json();
+
+    console.log(`Fetched ${show.name}.`);
+
+    return { show };
+}
 
 export default Post;
