ef4af12c83de33c3b5e7dcb16a39c79dfd4ea89c
Fabio Albertin <albertin@gmail.com>
2020-04-23 02:30:44 +0100

    Restarted tutorial for 9.3.5

---
+++ blog-app/nextjs-blog/pages/posts/first-post.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+const FirstPost = () => (
+  <>
+    <h1>First!</h1>
+    <p>
+      <Link href="/"><a>Home</a></Link>
+    </p>
+  </>
+);
+
+export default FirstPost;
