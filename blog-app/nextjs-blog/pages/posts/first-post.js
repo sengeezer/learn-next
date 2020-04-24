83b0c15b118f0750bdd549124dacc3059f8cef20
Fabio Albertin <albertin@gmail.com>
2020-04-24 02:43:13 +0100

    Started on styling

--- blog-app/nextjs-blog/pages/posts/first-post.js
+++ blog-app/nextjs-blog/pages/posts/first-post.js
@@ -1,12 +1,17 @@
+import Head from 'next/head';
+import Layout from '../../components/layout';
 import Link from 'next/link';
 
 const FirstPost = () => (
-  <>
+  <Layout>
+    <Head>
+      <title>First Post</title>
+    </Head>
     <h1>First!</h1>
     <p>
       <Link href="/"><a>Home</a></Link>
     </p>
-  </>
+  </Layout>
 );
 
 export default FirstPost;
